c1fad560c3622e5cf67dab930261747f100392b7
Rokhaya <seckr302@gmail.com>
2026-03-03 13:28:40 +0000

    Ajout de la page page_ weather_model

---
+++ lib/Model/page_weather_model.g.dart
@@ -0,0 +1,21 @@
+part of 'weather_model.dart';
+
+import 'package:untitled/Models/page_weather_model.dart';
+
+WeatherModel _$WeatherModelFromJson(Map<String, dynamic> json) =>
+    WeatherModel(
+      temperature: (json['temperature'] as num).toDouble(),
+      cityName: json['cityName'] as String,
+      description: json['description'] as String,
+      humidity: json['humidity'] as int,
+      windSpeed: (json['windSpeed'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$WeatherModelToJson(WeatherModel instance) =>
+    <String, dynamic>{
+      'temperature': instance.temperature,
+      'cityName': instance.cityName,
+      'description': instance.description,
+      'humidity': instance.humidity,
+      'windSpeed': instance.windSpeed,
+    };
---
+++ lib/Services/page_weather_service.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:untitled/models/page_weather_model.dart';
+
+import '../Models/page_weather_model.dart';
+
+class WeatherService {
+  final Dio _dio = Dio();
+  final String _apiKey = '57f02a136c29f2b7eeb1ddddb384b047';
+
+  Future<WeatherModel> getWeather(String cityName) async {
+    final response = await _dio.get(
+      'https://api.openweathermap.org/data/2.5/weather',
+      queryParameters: {
+        'q': cityName,
+        'appid': _apiKey,
+        'units': 'metric',
+        'lang': 'fr', // description en français
+      },
+    );
+
+    final data = response.data;
+
+    return WeatherModel(
+      temperature: (data['main']['temp'] as num).toDouble(),
+      cityName: data['name'],
+      description: data['weather'][0]['description'],
+      humidity: data['main']['humidity'],
+      windSpeed: (data['wind']['speed'] as num).toDouble(),
+    );
+  }
+}
